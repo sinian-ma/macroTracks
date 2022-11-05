8532215396a616c9014ba2aab8c251cf89124e67
“ian-ma-nyc” <sinma1995@gmail.com>
2022-11-04 22:34:31 -0400

    started login functionality

---
+++ client/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div className='header-container'>
+      <div className='left-head-container'>
+        <div className='logo'>MacroTracks</div>
+      </div>
+      <div className='right-head-container'>
+        <Link className='right-head-text' to={'/'}>
+          <div className='right-head-text'>Home</div>
+        </Link>
+        <Link className='right-head-text' to={'/user-information'}>
+          <div className='right-head-text'>User Information</div>
+        </Link>
+        <Link className='right-head-text' to={'/summary'}>
+          <div className='right-head-text'>Goals</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
