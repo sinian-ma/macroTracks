1402f9c427d5c921a18468447cb6b8f5c9810713
“ian-ma-nyc” <sinma1995@gmail.com>
2022-11-05 09:52:20 -0400

    added usermode and authcontroller

---
+++ server/models/userModels.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    status: {
+      type: String,
+      enum: ['Pending', 'Active'],
+      default: 'Pending',
+    },
+    confirmationCode: {
+      type: String,
+      unique: true,
+    },
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+      },
+    ],
+  })
+);
+
+module.exports = User;
